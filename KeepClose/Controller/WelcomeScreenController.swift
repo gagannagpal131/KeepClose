6bf7063d91be42f27ebba3d76c8b995949abba4a
Gagandeep Singh Nagpal <gagannagpal131@gmail.com>
2017-11-04 13:30:48 +0530

    Basic Layout is Ready

---
+++ KeepClose/Controller/WelcomeScreenController.swift
@@ -0,0 +1,35 @@
+//
+//  WelcomeScreenController.swift
+//  KeepClose
+//
+//  Created by Gagandeep Nagpal on 04/11/17.
+//  Copyright © 2017 rao. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeScreenController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
